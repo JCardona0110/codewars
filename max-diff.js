fc28aa05b8cf5e0bb3e7af768dac68c6f515359e
Jonathan Cardona <jcardona0110@gmail.com>
2023-02-27 07:03:10 -0500

    cube odd nums and sum

--- max-diff.js
+++ max-diff.js
@@ -4,13 +4,23 @@
 // if lst is empty or contains a single element, return 0
 // lst is not sorted
 
-function maxDiff(list) {
-  if(!list.length) return 0;
-  return Math.max(...list) - Math.min(...list);
-};
+// function maxDiff(list) {
+//   if(!list.length) return 0;
+//   return Math.max(...list) - Math.min(...list);
+// };
 
-// array of ints, can be empty
-// return
-// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
-// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
-maxDiff([1, 2, 3, 4] )
+// // array of ints, can be empty
+// // return
+// // [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// // [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+// maxDiff([1, 2, 3, 4] )
+
+const camelize = (str) =>
+	str
+		.split("-")
+		.map((word, index) =>
+			index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+		)
+		.join("");
+
+camelize("background-color");

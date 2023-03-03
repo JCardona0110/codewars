d0f15346f540d1cd9b67b37c7799c7355a903262
Jonathan Cardona <jcardona0110@gmail.com>
2023-03-03 07:15:49 -0500

    conditional slice to make nickname

---
+++ nickname-generator.js
@@ -0,0 +1,42 @@
+// Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+
+// If the 3rd letter is a consonant, return the first 3 letters.
+
+// nickname("Robert") //=> "Rob"
+// nickname("Kimberly") //=> "Kim"
+// nickname("Samantha") //=> "Sam"
+// If the 3rd letter is a vowel, return the first 4 letters.
+
+// nickname("Jeannie") //=> "Jean"
+// nickname("Douglas") //=> "Doug"
+// nickname("Gregory") //=> "Greg"
+// If the string is less than 4 characters, return "Error: Name too short".
+
+// Notes:
+
+// Vowels are "aeiou", so discount the letter "y".
+// Input will always be a string.
+// Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+// The input can be modified
+
+function nicknameGenerator(name) {
+	let splitName = [...name];
+	if (splitName.length < 4) return "Error: Name too short";
+	if (splitName[2].includes["a", "e", "i", "o", "u"]) {
+		return splitName.splice(0, 4).join("");
+	} else return splitName.splice(0, 3).join("");
+	// split into array
+	// splice first 3 letters if index 2 is a consonant
+	// splice first 4 letters otherwise
+	// to string
+	// return
+}
+nicknameGenerator("Joanne");
+// string with only letters
+// return
+// examples above
+
+// correct answer
+const nicknameGenerator2 = (name) => name.length > 3 
+  ? name.slice(0,  3 + 'aeiou'.includes(name[2])) 
+  : 'Error: Name too short'

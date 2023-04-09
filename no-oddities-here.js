de338a82444e48650982800a31f7d9967a0bf0c3
Jonathan Cardona <jcardona0110@gmail.com>
2023-04-09 17:09:47 -0400

    filter positive numbers

---
+++ no-oddities-here.js
@@ -0,0 +1,9 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+const noOdds = (value) => value.filter((num) => num % 2 === 0);
+
+// p- 0+
+// r-return all non odd values
+// e- [0,1,2,3] --> [0,2]

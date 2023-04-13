0a4f0ee4651db136d9d8263c5493d7a5d205f6bc
Jonathan Cardona <jcardona0110@gmail.com>
2023-04-12 23:54:05 -0400

    combine 2 strings using newest es6 method

---
+++ es6stringadd.js
@@ -0,0 +1,18 @@
+// It is easy to join two strings together like this string1 + string2.
+
+// Another way to get the desired result would be with string1.concat(string2)
+
+// ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+
+// + , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!
+
+// If one of the arguments is a number your code must coerce it into being a string.
+
+// p-2 strings
+// r-return
+// string1 = 'hi welcome'
+// string2 = 'my webapp'
+// combine to read 'hi welcome to my webapp'
+const joinStrings = (string1, string2) => `${string1} ${string2}`;
+//take in 2 strings
+// combine and return

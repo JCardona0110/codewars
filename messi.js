b6df6eb2db55d01a72c08340761a824a72295b54
Jonathan Cardona <131951890+JonathanCardona22@users.noreply.github.com>
2023-05-13 13:54:57 -0400

    new kata

---
+++ messi.js
@@ -0,0 +1,7 @@
+// Messi is a soccer player with goals in three leagues:
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+const goals = (a, b, c) => a + b + c;

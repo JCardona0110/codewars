bb807fcfa63fa9a994b4522d164b556631cff0c1
Jonathan Cardona <jcardona0110@gmail.com>
2023-07-19 00:03:07 -0400

    convert strings to nums and add then back to string

---
+++ sumthestrings.js
@@ -0,0 +1,34 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+// PREP ANSWER
+// p- 2 ints as string
+// r-return
+// e- "4",'5' --> '9'
+// p
+
+const sumStr = (a, b) => {
+	//   take in params and convert to nums
+	let sum = ~~a + ~~b;
+	return sum.toString();
+	//   add the params
+	//   convert back to string
+	//   return string
+};
+// CLEANER ANSWER
+const sumStr2 = (a, b) => (~~a + ~~b).toString();
+
+// CLEVER ANSWER
+sumStr = (a, b) => String(+a + +b);

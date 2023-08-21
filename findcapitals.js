32589fbde3eaecc4a69395238553f422f31319ac
Jonathan Cardona <jcardona0110@gmail.com>
2023-08-21 11:17:51 -0400

    return indexes of capital letters

---
+++ findcapitals.js
@@ -0,0 +1,22 @@
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// let capitals = (word) => {
+// let result = [];
+// word.forEach((e) =>
+// 	e === e.toUpperCase() ? (result = e.push()) : (result = [])
+// );
+// };
+
+// p-string of letters
+// r-return array
+// e-"CodEWaRs" --> [0,3,4,6]
+
+// what i was going for
+
+let capitals = function (word) {
+	let result = [];
+	for (let i = 0; i < word.length; i++) {
+		if (word[i].toUpperCase() == word[i]) caps.push(i);
+	}
+	return caps;
+};

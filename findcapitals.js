382c957e33c5257f8f09bec4847665216708ab4d
Jonathan Cardona <jcardona0110@gmail.com>
2023-09-13 18:55:55 -0400

    fixed variable

--- findcapitals.js
+++ findcapitals.js
@@ -16,7 +16,7 @@
 let capitals = function (word) {
 	let result = [];
 	for (let i = 0; i < word.length; i++) {
-		if (word[i].toUpperCase() == word[i]) caps.push(i);
+		if (word[i].toUpperCase() == word[i]) result.push(i);
 	}
 	return result;
 };
